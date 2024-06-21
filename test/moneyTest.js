b8d3c77144b16cb5c925d8a23bd7a607b9b50172
MiniGT <bendanillo539@gmail.com>
2024-06-21 14:14:30 +0800

    added jasmine testing framework

--- test/moneyTest.js
+++ test/moneyTest.js
@@ -1,5 +1,7 @@
 import { formatCurrency } from "../scripts/utils/money.js";
 
+console.log("test suite: formatCurrency");
+
 if (formatCurrency(2095) === "20.95") {
   console.log("passed");
 } else {
