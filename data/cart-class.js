c39c34cb5d1f7ba5587ad1cbb02184a3c5b121cd
MiniGT <bendanillo539@gmail.com>
2024-07-24 12:19:36 +0800

    fixed saveStorage

--- data/cart-class.js
+++ data/cart-class.js
@@ -45,7 +45,7 @@ class Cart {
       const index = this.checkCartIndex(productId);
       this.cartItems[index].quantity += optionValue;
     }
-    saveToStorage(this.cartItems);
+    this.saveToStorage(this.cartItems);
   }
 
   checkCartIndex(productId) {
@@ -84,7 +84,7 @@ class Cart {
   }
 }
 
-const cart = new Cart("cart-oop");
+export const cart = new Cart("cart-oop");
 const businessCart = new Cart("cart-business");
 
 console.log(cart);
