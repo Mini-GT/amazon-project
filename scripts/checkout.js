e94c4f967a3ef3e2750432cc559b6467aeb19d68
MiniGT <bendanillo539@gmail.com>
2024-07-27 11:47:01 +0800

    convert promises to async await

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -8,75 +8,107 @@ import "../data/car.js";
 //import '../data/backend-practice.js'
 import '../data/products.js'
 
-//promise runs the func immediately
-/* new Promise((resolve) => {
-  console.log('starts promise')
-  loadProducts(() => {
-    console.log('finished loading')
-    resolve();
-  });
-}).then(() => {
-  console.log('next step')
-})
+// async makes a function return a promise
+// this is the same as:
+// function loadPage() {
+//   return new Promise((resolve) => {
+//     console.log('load  page')
+//     resolve();
+//   })
+// }
 
-new Promise((resolve) => {
-  loadProducts(() => {
-    resolve();
-  });
-}).then(() => {
-  renderOrderSummary();
-  renderPaymentSummary();
-  renderCheckoutHeaderMiddileSection();
-});
+async function loadPage() {
+  // console.log('load page async')
 
- loadProducts(() => {
-  renderOrderSummary();
-  renderPaymentSummary();
-  renderCheckoutHeaderMiddileSection();
-});
+  //await lets us wait for the function to finish (no more .then())
+  await loadProductsFetch();
 
-new Promise((resolve) => {
-  loadProducts(() => {
-    //resolve will pass this value1 into .then()
-    resolve('value1');
-  });
-
-}).then((value) => {
-  console.log(value);
-
-  return new Promise((resolve) => {
+  await new Promise((resolve) => {
     loadCart(() => {
-      resolve('value2');
+      resolve();
     });
-  });
-
-}).then((value2) => {
-  console.log(value2);
+  }); 
 
   renderOrderSummary();
   renderPaymentSummary();
   renderCheckoutHeaderMiddileSection();
-}); */
+  return 'value2' //basically same as resolve('value2') where we pass the value as argument to .then() parameter
+}
+loadPage()
+// .then((value) => {
+//   console.log('next step async')
+//   console.log(value);
+// })
+
+//promise runs the func immediately
+// new Promise((resolve) => {
+//   console.log('starts promise')
+//   loadProducts(() => {
+//     console.log('finished loading')
+//     resolve();
+//   });
+// }).then(() => {
+//   console.log('next step')
+// })
+
+// new Promise((resolve) => {
+//   loadProducts(() => {
+//     resolve();
+//   });
+// }).then(() => {
+//   renderOrderSummary();
+//   renderPaymentSummary();
+//   renderCheckoutHeaderMiddileSection();
+// });
+
+//  loadProducts(() => {
+//   renderOrderSummary();
+//   renderPaymentSummary();
+//   renderCheckoutHeaderMiddileSection();
+// });
+
+// new Promise((resolve) => {
+//   loadProducts(() => {
+//     //resolve will pass this value1 into .then()
+//     resolve('value1');
+//   });
+
+// }).then((value) => {
+//   console.log(value);
+
+//   return new Promise((resolve) => {
+//     loadCart(() => {
+//       resolve('value2');
+//     });
+//   });
+
+// }).then((value2) => {
+//   console.log(value2);
+
+//   renderOrderSummary();
+//   renderPaymentSummary();
+//   renderCheckoutHeaderMiddileSection();
+// });
 
 //Promise.all() can run multiple promises at the same time
-Promise.all([
-  /* new Promise((resolve) => {
-    loadProductsFetch(() => {
-      //resolve will pass this value1 into .then()
-      resolve();
-    });
-  }) 
-  */
-  // loadProductsFetch returns a promise so we can just run the function instead above step
-  loadProductsFetch(),
-  new Promise((resolve) => {
-    loadCart(() => {
-      resolve();
-    });
-  }),
+//Promise.all([
+// new Promise((resolve) => {
+//   loadProductsFetch(() => {
+//     //resolve will pass this value1 into .then()
+//     resolve();
+//   });
+// }) 
 
-]).then(() => {
-  renderOrderSummary();
-  renderPaymentSummary();
-  renderCheckoutHeaderMiddileSection();
-});
+// loadProductsFetch returns a promise so we can just run the function instead above step
+//   loadProductsFetch(),
+//   new Promise((resolve) => {
+//     loadCart(() => {
+//       resolve();
+//     });
+//   }),
+
+// ]).then(() => {
+//   renderOrderSummary();
+//   renderPaymentSummary();
+//   renderCheckoutHeaderMiddileSection();
+// });
